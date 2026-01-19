377e445ed41d588c36e5c30997bf8fca0307c6c1
DaniRohmatulloh <rohmatullohdani48@gmail.com>
2026-01-19 15:10:15 +0700

    DFD dan Flowchart(belum selesai)

--- Pengembangan-sistem-ujian/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ Pengembangan-sistem-ujian/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,13 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $user = Auth::user();
+        
+        if ($user->role == 'guru') {
+            return redirect('/guru/dashboard');
+        } else {
+            return redirect('/siswa/dashboard');
+        }
     }
 
     /**
@@ -45,3 +51,4 @@ class AuthenticatedSessionController extends Controller
         return redirect('/');
     }
 }
+
--- Pengembangan-sistem-ujian/routes/web.php
+++ Pengembangan-sistem-ujian/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ExamController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\GuruController;
+use App\Http\Controllers\SiswaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,10 +15,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/guru/dashboard', [GuruController::class, 'index']);
+    Route::get('/siswa/dashboard', [SiswaController::class, 'index']);
 });
 
+
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
+
+    Route::get('/exam/{id}', [ExamController::class,'start']);
+    Route::post('/exam/{id}/submit', [ExamController::class,'submit']);
+
+});
